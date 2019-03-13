c505636f17d93f9fe1c97c7e3eaf2ad6c882c4e0
emaunzpa <emaunzparkitny@excilys.com>
2019-03-13 14:50:59 +0100

    Modification de la fonction main pour le lancement de l'interface en ligne de commande.

--- src/com/emaunzpa/computerdatabase/service/TestJDBC.java
+++ src/com/emaunzpa/computerdatabase/service/TestJDBC.java
@@ -1,5 +1,7 @@
 package com.emaunzpa.computerdatabase.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -10,25 +12,11 @@ import com.emaunzpa.computerdatabase.model.Manufacturer;
 
 public class TestJDBC {
 
-	public static void main(String[] args) {
-		
-//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-//		
-//		ComputerDriver cptDriver = new ComputerDriver();
-//		ManufacturerDriver mfDriver = new ManufacturerDriver();
-//		
-//		LocalDateTime currentTime = LocalDateTime.now();
-//		LocalDateTime dateTime = currentTime.withYear(currentTime.getYear() + 2);
-//		String currentDateTime = "'" + currentTime.format(formatter) +"'";
-//		String currentDateTimeModified = "'" + dateTime.format(formatter) + "'";
-//		
-//		Manufacturer manu = mfDriver.getManufacturer(33);
-//	    cptDriver.updateComputer(575, "'Titi acer v3.0'", currentDateTime, currentDateTimeModified, 1);
-//		
-//		System.out.println(manu.getName());
-		
+	public static void main(String[] args) throws ParseException {
+				
 		CommandeLigneInterface cli = new CommandeLigneInterface();
 		cli.run();
+		
 	}
 
 }
