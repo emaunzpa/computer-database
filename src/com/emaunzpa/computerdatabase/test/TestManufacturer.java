4851ea8ebcf2acfd779c45d6498bcd2b6b559de0
emaunzpa <emaunzparkitny@excilys.com>
2019-03-14 15:44:01 +0100

    Sprint 2 - Junit Added : TestCompany testing all methods related to companies handling.

---
+++ src/com/emaunzpa/computerdatabase/test/TestManufacturer.java
@@ -0,0 +1,32 @@
+package com.emaunzpa.computerdatabase.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.emaunzpa.computerdatabase.bdd.ComputerDriver;
+import com.emaunzpa.computerdatabase.bdd.ManufacturerDriver;
+import com.emaunzpa.computerdatabase.model.Manufacturer;
+import com.emaunzpa.computerdatabase.util.DatesHandler;
+
+public class TestManufacturer {
+
+	private ManufacturerDriver manufacturerDriver;
+	
+	@Before
+	public void createComputerDriver() {
+		manufacturerDriver = new ManufacturerDriver();
+	}
+	
+	@Test
+	public void getAllManufacturers() {
+		assertNotNull(manufacturerDriver.getAllManufacturers());
+	}
+	
+	@Test
+	public void getManufacturer() {
+		assertEquals("Apple Inc.", manufacturerDriver.getManufacturer(1).getName());
+	}
+
+}
