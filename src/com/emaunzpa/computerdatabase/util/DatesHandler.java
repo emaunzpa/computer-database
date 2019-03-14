27701837f0cbf7f59cd7abd16f4914e02eb352d9
emaunzpa <emaunzparkitny@excilys.com>
2019-03-14 11:50:58 +0100

    Added : datesHandler for dates convertion. Added : enumeration for easier understanding of switch cases in the controller.

---
+++ src/com/emaunzpa/computerdatabase/util/DatesHandler.java
@@ -0,0 +1,34 @@
+package com.emaunzpa.computerdatabase.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class DatesHandler {
+
+	private SimpleDateFormat sdf;
+	
+	/**
+	 * Creator without parameter
+	 */
+	public DatesHandler() {
+		this.sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	}
+	
+	/**
+	 * Convert a date in String format to an SQL date
+	 * @param stringDate
+	 * @return sqlDate
+	 */
+	public java.sql.Date convertStringDateToSqlDate(String stringDate){
+		java.util.Date stringDateParsed = null;
+		try {
+			stringDateParsed = sdf.parse(stringDate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	java.sql.Date sqlDate = new java.sql.Date(stringDateParsed.getTime());
+    	return sqlDate;
+	}
+	
+}
